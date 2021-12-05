2c576ed292ebb36cc9be08edec06c45871934dea
Rolf <rolfschellenberger@gmail.com>
2021-12-05 08:31:33 +0100

    Day 05

--- src/main/kotlin/com/rolf/day05/OceanFloor.kt
+++ src/main/kotlin/com/rolf/day05/OceanFloor.kt
@@ -1,6 +1,7 @@
 package com.rolf.day05
 
 import com.rolf.Matrix
+import java.lang.Math.abs
 
 data class OceanFloor(val input: List<MutableList<Int>>) : Matrix<Int>(input) {
     fun place(pipe: Pipe) {
@@ -12,18 +13,18 @@ data class OceanFloor(val input: List<MutableList<Int>>) : Matrix<Int>(input) {
     }
 
     private fun getPointsBetween(start: Point, end: Point): List<Point> {
-        val minX = minOf(start.x, end.x)
-        val maxX = maxOf(start.x, end.x)
-        val minY = minOf(start.y, end.y)
-        val maxY = maxOf(start.y, end.y)
-        val range = maxOf((maxX - minX), (maxY - minY))
-
         val points = mutableListOf<Point>()
         val yDelta = if (end.y > start.y) 1 else if (end.y == start.y) 0 else -1
         val xDelta = if (end.x > start.x) 1 else if (end.x == start.x) 0 else -1
-        for (i in 0..range) {
+        for (i in 0..getRange(start, end)) {
             points.add(Point(start.x + (i * xDelta), start.y + (i * yDelta)))
         }
         return points
+    }
+
+    private fun getRange(start: Point, end: Point): Int {
+        val xRange = abs(start.x - end.x)
+        val yRange = abs(start.y - end.y)
+        return maxOf(xRange, yRange)
     }
 }
